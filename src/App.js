4f2b16ef161bdf632c9b07fedf76a86b616c6ad4
Tikay <kiet211194@gmail.com>
2020-04-20 14:22:27 +0700

    lesson4-useState-b

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
-import ColorBox from "./components/ColorBox";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Hello" },
+    { id: 2, title: "Hi" },
+    { id: 3, title: "Hey" },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
   return (
     <div className="app">
       <p>Welcome to Hooks Basic</p>
 
-      <ColorBox />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
