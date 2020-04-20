b56ee25ae1d6a61fd7e8de4b57f4b17bb326e990
Tikay <kiet211194@gmail.com>
2020-04-20 13:46:38 +0700

    lesson3-useState-a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.scss";
+import ColorBox from "./components/ColorBox";
 
 function App() {
   return (
     <div className="app">
       <p>Welcome to Hooks Basic</p>
+
+      <ColorBox />
     </div>
   );
 }
