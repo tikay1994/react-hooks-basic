a94321cabec4ad9282a8273ea745badc3239d334
Tikay <kiet211194@gmail.com>
2020-04-20 10:41:09 +0700

    Begin-React-Hooks

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <p>Welcome to Hooks Basic</p>
+    </div>
+  );
+}
+
+export default App;
